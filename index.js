19513eb83812224a5b91bea6ccf9cc2821622d26
sakin111 <maliksakin53@gmail.com>
2024-09-07 14:04:14 +0600

    modified

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ const port = process.env.PORT || 5000;
 
 
 app.use(express.json())
-app.use(cors({
-  origin: ['https://earnest-cactus-351358.netlify.app', 'http://localhost:5173/'],
-  credentials:true, 
-}));
+// app.use(cors({
+//   origin: ['https://earnest-cactus-351358.netlify.app', ],
+//   credentials:true, 
+// }));
 // app.use(cors())
+
+app.use(cors({
+  origin: ['https://earnest-cactus-351358.netlify.app','http://localhost:5173/'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE','UPDATE']
+}));
 
 
 const uri = `mongodb+srv://${process.env.MONGO_user}:${process.env.MONGO_pass}@cluster0.ubtwufv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
