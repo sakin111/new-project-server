7fe5104ecdb85f0cf1c70916f0c05ba9d360a14f
sakin111 <maliksakin53@gmail.com>
2024-09-10 11:37:52 +0600

    git commited

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors({
-  origin: ['https://earnest-cactus-351358.netlify.app','http://localhost:5173/' ],
+  origin: ['https://earnest-cactus-351358.netlify.app', 'http://localhost:5173'],
   credentials:true, 
 }));
 // app.use(cors())
