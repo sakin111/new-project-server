20f077b1cd71bed8656ce819a11a4f997ae6f290
sakin111 <maliksakin53@gmail.com>
2024-09-10 09:54:15 +0600

    fixing cors policy

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const port = process.env.PORT || 5000;
 
 
 
-app.use(express.json())
-app.use(cors({
-  origin: ['https://earnest-cactus-351358.netlify.app','http://localhost:5173/' ],
-  credentials:true, 
-}));
+// app.use(express.json())
+// app.use(cors({
+//   origin: ['https://earnest-cactus-351358.netlify.app','http://localhost:5173/' ],
+//   credentials:true, 
+// }));
+app.use(cors())
 
 
 
@@ -115,6 +116,49 @@ const verifyToken = (req, res, next) => {
       const result = await addToCart.insertOne(ToCart);
       res.send(result);
     });
+
+
+// payment getWay
+
+app.post("/create-payment" ,async(req,res) =>{
+const paymentInfo = req.body;
+const initiateData = {
+  store_id: 'devel66dd635867967',
+  store_passwd: 'devel66dd635867967@ssl',
+  total_amount: 100,
+  currency: 'BDT',
+  tran_id: 'REF123',
+  success_url: "http://yoursite.com/success.php",
+  fail_url: "http://yoursite.com/fail.php",
+  cancel_url: "http://yoursite.com/cancel.php",
+  cus_name: "Customer Name",
+  cus_email: "cust@yahoo.com",
+  cus_add1: 'Dhaka',
+  cus_add2: "Dhaka",
+  cus_city: "Dhaka",
+  cus_state: "Dhaka",
+  cus_postcode: 1000,
+  cus_country: "Bangladesh",
+  cus_phone: "01711111111",
+  cus_fax: "01711111111",
+  ship_name: "Customer Name",
+  ship_add1: "Dhaka",
+  ship_add2: "Dhaka",
+  ship_city: 'Dhaka',
+  ship_state: "Dhaka",
+  ship_postcode: 1000,
+  ship_country: "Bangladesh",
+  multi_card_name: "mastercard,visacard,amexcard",
+  value_a: "ref001_A",
+  value_b: "ref002_B",
+  value_c: 'ref003_C',
+  value_d: "ref004_D"
+}
+
+
+res.send(result)
+})
+
 
 
 
